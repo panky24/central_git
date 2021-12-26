41839427996ab4af964f29a64cbed4af946ef905
pankaj2 <pankaj.singh@gmail.com>
2021-12-27 03:20:21 +0530

    added .py files

---
+++ example_if_else_condition.py
@@ -0,0 +1,8 @@
+name = input("Enter your name:")
+print("Hello, " +name)
+age = input("Please enter your age:")
+age = int(age)
+if age > 3:
+    print("You are allowed to use the internet.")
+elif age <= 3:
+    print("You are still a kid what are you doing here.")
