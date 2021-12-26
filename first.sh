20d9bc19c630445c4ddbef541e95cd0247d077af
panky24 <panky.pankusn@gmail.com>
2021-12-27 02:21:38 +0530

    added more

--- first.sh
+++ first.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 echo "Hello World"
+
+
+###### echo command ############
+#!/bin/bash
+echo "Printing text with newline"
+echo -n "Printing text without newline"
+echo -e "\nRemoving \t backslash \t characters\n"
+
+
+##using while loop
+#!/bin/bash
+valid=true
+count=1
+while [ $valid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+((count++))
+done
+
+
